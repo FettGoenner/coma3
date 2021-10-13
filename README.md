118dfec9378cfc3b38ae756dbd679382f784be39
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 12:45:12 +0200

    Readme erweitert, gitignore geleert

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Kollaboratives Projekt für das Modul Computerorientierte Mathematik III der BHT.
 Irgendwas mit Qt und C++.
 
+### Anmerkungen von Paul
+Ich kompiliere C++-Anwendungen manuell mit g++.
+
 ### Lizenz
 GPLv3
--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ using namespace std ;
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    QLabel hello("Hello woow!");
+    QLabel hello("Hello World!");
 
     hello.show();
     return app.exec();
