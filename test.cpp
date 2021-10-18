46a6d4b6b3f3f3cc20a09fd7886dfb5ead8f75c5
FettGoenner <paulmatti@protonmail.com>
2021-10-18 14:07:06 +0200

    Besserer Stil

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@ using namespace std ;
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    QLabel hello("Hello World!");
+    QApplication app( argc, argv ) ;
+    QLabel hello( "Hello World!" ) ;
 
     hello.show();
     return app.exec();
