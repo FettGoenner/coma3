9cbca4ff322d434eed161bb375d1619c03fbba98
GlooMyPan <p353180107@gmail.com>
2021-12-15 19:51:12 +0100

    finished newGameDialogView

--- savegameview.cpp
+++ savegameview.cpp
@@ -6,8 +6,35 @@ SaveGameDialog::SaveGameDialog(QWidget *parent) :
     ui(new Ui::SaveGameDialog)
 {
     ui->setupUi(this);
-    ui->gameFileTable->setHorizontalHeaderLabels(QStringList() << "File Name" << "Create Time");
+    ui->gameFileTable->setColumnCount(2);
+    ui->gameFileTable->setHorizontalHeaderLabels(QStringList() << "File Name" << "Update Time");
     ui->gameFileTable->setRowCount(3);
+    ui->gameFileTable->setItem(0, 0, new QTableWidgetItem("111"));
+    ui->gameFileTable->setItem(1, 0, new QTableWidgetItem("222"));
+    ui->gameFileTable->setItem(2, 0, new QTableWidgetItem("333"));
+    // select by rows
+    ui->gameFileTable->setSelectionBehavior(QTableWidget::SelectRows);
+
+    // cannot select multiple rows
+    ui->gameFileTable->setSelectionMode(QTableWidget::SingleSelection);
+
+    // cannot edit the cells of the table
+    ui->gameFileTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->gameFileTable->setHorizontalHeaderLabels(QStringList() << "File Name" << "Create Time");
+    ui->deleteBtn->setEnabled(false);
+    connect(ui->gameFileTable, &QTableWidget::cellClicked, this, [=]() {
+        ui->deleteBtn->setEnabled(true);
+    });
+    connect(ui->addBtn, &QPushButton::clicked, this, [=]() {
+        if (ui->gameFileTable->currentRow() == -1)
+            ui->gameFileTable->insertRow(ui->gameFileTable->rowCount());
+        else
+            ui->gameFileTable->insertRow(ui->gameFileTable->currentRow() + 1);
+    });
+
+    connect(ui->deleteBtn, &QPushButton::clicked, this, [=]() {
+        ui->gameFileTable->removeRow(ui->gameFileTable->currentRow());
+    });
 }
 
 void SaveGameDialog::resizeEvent(QResizeEvent *)
